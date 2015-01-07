d9d2ae44a6cbb552c83a7deeacb09a4560283d6c
Norm MacLennan <norm.maclennan@gmail.com>
2015-01-06 19:15:12 -0500

    kind of working with osquery

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,11 @@
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
 #include <facter/facts/collection.hpp>
+#include <osquery/core.h>
+#include <osquery/core/virtual_table.h>
+#include <osquery/sql.h>
+#include <osquery/database.h>
+#include <boost/property_tree/json_parser.hpp>
 
 typedef websocketpp::client<websocketpp::config::asio_client> client;
 using websocketpp::lib::placeholders::_1;
@@ -12,23 +17,43 @@ typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
 
 facter::facts::collection facts;
 void on_message(client* c, websocketpp::connection_hdl hdl, message_ptr msg) {
-    if(msg->get_payload() != "hostname") return; // just because this is our hard-coded query
+    boost::property_tree::ptree message_payload;
+    std::istringstream ss(msg->get_payload());
+    boost::property_tree::read_json(ss, message_payload);
 
-    client::connection_ptr con = c->get_con_from_hdl(hdl);
-    std::cout << "Query " << msg->get_header() << " " <<  msg->get_payload() << std::endl;
+    std::string action = message_payload.get<std::string>("action", "");
+    std::string payload = message_payload.get<std::string>("payload", "");
 
-    std::set<std::string> query = {msg->get_payload()};
-    std::stringstream value;
-    facts.write(value, facter::facts::format::json, query);
+    if(action == "query"){
+        auto sql = osquery::SQL(payload);
+        if(sql.ok()){
+            client::connection_ptr con = c->get_con_from_hdl(hdl);
+            boost::property_tree::ptree results_payload;
+            osquery::serializeQueryData(sql.rows(), results_payload);
+            std::ostringstream os;
+            boost::property_tree::write_json(os, results_payload, false);
+            std::string response_message = "{\"action\": \"response\", \"payload\": " + os.str() + "}";
+            con->send(response_message);
+        }
+    }
 
-    con->send(value.str());
+    if(action == "fact"){
+        client::connection_ptr con = c->get_con_from_hdl(hdl);
+        std::stringstream value;
+        std::set<std::string> query = {payload};
+        facts.write(value, facter::facts::format::json, query);
+        std::string response_message = "{\"action\": \"response\", \"payload\": " + value.str() + "}";
+        con->send(response_message);
+    }
 }
 
 void on_connect(client* c, websocketpp::connection_hdl hdl){
     std::cout << "Waiting for queries..." << std::endl;
     client::connection_ptr con = c->get_con_from_hdl(hdl);
 
-    con->send("hostname");
+    std::string register_message = "{\"action\": \"register_server\", \"payload\": \"" + osquery::getHostname() + "\"}";
+
+    con->send(register_message);
 }
 
 void on_disconnect(client* c, websocketpp::connection_hdl hdl){
@@ -38,9 +63,9 @@ void on_disconnect(client* c, websocketpp::connection_hdl hdl){
 
 
 int main(int argc, char* argv[]) {
-
+    // Initialize our sources
+    osquery::initOsquery(argc,argv);
     facts.add_default_facts();
-    std::set<std::string> stuff = {"hostname"};
 
     client c;
     std::string uri = "ws://localhost:8080";
--- server/index.js
+++ server/index.js
@@ -6,11 +6,51 @@ var WebSocketServer = require('ws').Server
 app.use(express.static(__dirname + '/public'));
 app.listen(8080);
 
+var servers = [];
+var clients = [];
+
 var wss = new WebSocketServer({server: app});
 wss.on('connection', function(ws) {
 
     // just immediately re-broadcast any messages we get.
     ws.on('message', function(message){
+
+        console.log(message);
+
+        var msg = JSON.parse(message);
+        if(msg.action === "register_server"){
+            servers.push({"hostname": msg.payload, "connection": ws});
+            console.log("registered server " + msg.payload);
+
+            return;
+        }
+
+        if(msg.action === "register_client"){
+            clients.push({"id": msg.payload, "connection": ws});
+            console.log("registered client");
+
+            return;
+        }
+
+        if(msg.action === "query" || msg.action === "fact"){
+            servers.forEach(function(c){
+                c.connection.send(message);
+            });
+
+            console.log("executing query");
+
+            return;
+        }
+
+        if(msg.action === "response"){
+            clients.forEach(function(c){
+                c.connection.send(message);
+            });
+
+            console.log("query response");
+            return;
+        }
+
         console.log(message);
         wss.broadcast(message);
     });
