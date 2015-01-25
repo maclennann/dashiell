6a45fd32bfe6162fbae7aaff0d0d9805f3d35d8a
Norm MacLennan <norm.maclennan@gmail.com>
2015-01-25 11:19:50 -0500

    updating provision script to new deb

--- provision.sh
+++ provision.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 wget http://nightlies.puppetlabs.com/cfacter-latest/repo_configs/deb/pl-cfacter-latest-trusty.list -O /etc/apt/sources.list.d/pl-cfacter-latest-trusty.list
+echo "deb http://dl.bintray.com/maclennann/deb /" > /etc/apt/sources.list.d/dashiell.list
+
 apt-get update
-#wget https://bintray.com/artifact/download/maclennann/deb/dashiell-0.1.1.deb -O /vagrant/dashiell-0.1.1.deb
-#dpkg -i /vagrant/dashiell-0.1.1.deb
-#apt-get install -f -y
-sudo apt-get install npm cfacter --force-yes -y
+sudo apt-get install npm dashiell --force-yes -y
+
 ln -s /usr/bin/nodejs /usr/bin/node
